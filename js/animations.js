c89da40a70b6cb1d3783dd235a519feea2b875a9
andrewplan <a.plan88@gmail.com>
2016-09-02 19:15:03 -0500

    Finished all steps through Black Diamond.

--- js/animations.js
+++ js/animations.js
@@ -1,24 +1,25 @@
 $( document ).ready( function() {
+  // localStorage[ 'allTheTweets' ] = '';
+
   var tweets = [];
   var tweetCompose = $( '.tweet-compose' );
   var tweetControls = $( '#tweet-controls' );
   var charCount = $( '#char-count' );
   var tweetSubmit = $( '#tweet-submit' );
   var myFullName = $( '#myFullName' ).text();
-  var myHandle = $( '#myHandle' ).text();
+  var myHandle = '@andrewplan';
   var myAvatarSrc = 'img/alagoon.jpg';
   var currentTime = new Date();
   var currentTimeInISO = currentTime.toISOString();
 
   jQuery("time.timeago").timeago();
   $( '.tweet-actions' ).animate( {opacity: 0}, 0 );
-
   grabDataFromLocalStorage();
 
   function grabDataFromLocalStorage() {
     // if localStorage is not empty, parse it and store in listo.
-    if ( typeof localStorage[ 'allTweets' ] !== 'undefined' && localStorage[ 'allTweets' ] !== '' ) {
-      tweets = JSON.parse( localStorage[ 'allTweets' ] );
+    if ( typeof localStorage[ 'allTheTweets' ] !== 'undefined' && localStorage[ 'allTheTweets' ] !== '' ) {
+      tweets = JSON.parse( localStorage[ 'allTheTweets' ] );
       repopulateTweetStream( tweets );
     }
     // if localStorage is empty, set listo to equal []
@@ -27,8 +28,75 @@ $( document ).ready( function() {
     }
   }
 
-  function repopulateTweetStream() {
+  function repopulateTweetStream( arr ) {
+    for ( var i = 0; i < arr.length; i++ ) {
+      $( '#stream' ).prepend(
+        '<div class="tweet">' +
+          '<div class="content">' +
+            '<img class="avatar" src="' + myAvatarSrc + '" data-title="@andrewplan"/>' +
+            '<div id="favorite-active" class="status-icon-wrapper">' +
+              '<img class="status-icon" src="img/retweet-icon-1.png">' +
+            '</div>' +
+            '<div id="retweet-active" class="status-icon-wrapper">' +
+              '<img class="status-icon" src="img/favorite-status-icon.png">' +
+            '</div>' +
+            '<strong class="fullname">' + myFullName + '</strong>' +
+            '<span class="username">' + ' ' + myHandle +  '</span>' +
+            '<!-- BLACK DIAMOND: Implement the icons for when a tweet is favorited/retweeted in the upper right of the tweet. -->' +
+            '<!-- HINT: Add to what you created in Step 5 -->' +
+            '<p class="tweet-text">' + arr[ i ][ 'tweet' ] + '</p>' +
+            '<!-- STEP 6: The tweet actions below should only show up when you hover over the tweet. Otherwise, they should be hidden. -->' +
+            '<!-- HINT: CSS ":hover" pseudo element -->' +
+            '<div class="tweet-actions">' +
+              '<ul>' +
+                '<li><span class="icon action-reply"></span> Reply</li>' +
+                '<li><span class="icon action-retweet"></span> Retweet</li>' +
+                '<li><span class="icon action-favorite"></span> Favorite</li>' +
+                '<li><span class="icon action-more"></span> More</li>' +
+              '</ul>' +
+            '</div>' +
+            '<!-- STEP 7: The Retweets/timestamp/Reply areas below should also be hidden by default. These should only expand if you click on the tweet. Use a jQuery animation to accomplish the reveal, similar to how it’s done on Twitter.com -->' +
+            '<!-- HINT: jQuery ".on" eventHandler -->' +
+            '<div class="stats">' +
+              '<div class="retweets">' +
+                '<p class="num-retweets">30</p>' +
+                '<p>RETWEETS</p>' +
+              '</div>' +
+              '<div class="favorites">' +
+                '<p class="num-favorites">6</p>' +
+                '<p>FAVORITES</p>' +
+              '</div>' +
+              '<div class="users-interact">' +
+                '<div>' +
+                  '<!-- BLACK DIAMOND: Implement the Bootstrap tooltips for when you hover over a user\’s avatar image -->' +
+                  '<!-- HINT: Bootstrap has good documentation -->' +
+                  '<img class="avatar" src="img/alagoon.jpg" data-title="@andrewplan" />' +
+                  '<img class="avatar" src="img/vklimenko.jpg" data-title="@iliketheletterV" />' +
+                '</div>' +
+              '</div>' +
+              '<!-- BLACK DIAMOND: Make the timestamp below similar to how they look on Twitter (1h, 18m, 1m) and use the jQuery timeago plugin to make them automatic. -->' +
+              '<!-- HINT: Refer to timeago documentation -->' +
+              '<div class="time">' +
+                  '<time class="timeago" datetime="' + arr[ i ][ 'date' ] +
+                  '">' +
+                      // currentTime +
+                  '</time>' +
+              '</div>' +
+            '</div>' +
+            '<div class="reply">' +
+              '<img class="avatar" src="img/alagoon.jpg" data-title="@andrewplan" />' +
+              '<textarea class="tweet-compose" placeholder="Reply to @mybff"/></textarea>' +
+            '</div>' +
+          '</div>' +
+        '</div>'
+      );
+    }
+  }
 
+  function TweetGenerator( tweet, date, handle ) {
+      this.tweet = tweet;
+      this.date = date;
+      this.handle = handle;
   }
 
   // When the user clicks on the textarea, the textarea should double in size and the character count and Tweet buttons should be revealed.
@@ -64,6 +132,9 @@ $( document ).ready( function() {
   tweetSubmit.click( function(){
     // gets tweet text and stores to a variable
     var tweetContent = $( '.tweet-compose' ).val();
+    var tweet = new TweetGenerator( tweetContent, currentTimeInISO, myHandle);
+    tweets.push( tweet );
+    localStorage[ 'allTheTweets' ] = JSON.stringify( tweets );
 
     // prepends div containing tweet contents
     $( '#stream' ).prepend(
@@ -77,7 +148,7 @@ $( document ).ready( function() {
             '<img class="status-icon" src="img/favorite-status-icon.png">' +
           '</div>' +
           '<strong class="fullname">' + myFullName + '</strong>' +
-          '<span class="username">' + myHandle +  '</span>' +
+          '<span class="username">' + ' ' + myHandle +  '</span>' +
           '<!-- BLACK DIAMOND: Implement the icons for when a tweet is favorited/retweeted in the upper right of the tweet. -->' +
           '<!-- HINT: Add to what you created in Step 5 -->' +
           '<p class="tweet-text">' + tweetContent + '</p>' +
@@ -142,7 +213,7 @@ $( document ).ready( function() {
 
   // The Retweets/timestamp/Reply areas should also be hidden by default. These should only expand if you click on the tweet. Have the students use a jQuery animation to accomplish the reveal, similar to how it’s done on Twitter.com
 
-  $( document ).on( 'click', '.tweet', function( event) {
+  $( document ).on( 'click', '.tweet', function( event ) {
     if ( !$( 'li, this' ).is( event.target ) ) {
       $( this ).find( '.stats, .reply' ).slideToggle();
     }
